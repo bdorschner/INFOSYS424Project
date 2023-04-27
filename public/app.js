0f7a54f37dc08cdedbc8d4ec87c09281bc1bbbf7
Jacob <JBRinghand2001@gmail.com>
2023-04-27 16:18:38 -0500

    - fixed authentication to work on desktop - and mobile

--- public/app.js
+++ public/app.js
@@ -215,7 +215,26 @@ if (user) {
 
 // handle sign in button click event
 loginButton.addEventListener('click', () => {
-auth.signInWithRedirect(provider);
+  // Use signInWithPopup instead of signInWithRedirect
+  auth.signInWithPopup(provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = result.credential;
+      const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+      
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = error.credential;
+    });
 });
 
 // handle sign out button click event
